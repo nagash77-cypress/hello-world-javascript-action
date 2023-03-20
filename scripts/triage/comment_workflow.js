b79cc20703579ceb4eb124f31804746b87100db0
Ben M <benm@cypress.io>
2023-03-20 15:24:16 -0400

    chore: changing actions sctructure

--- scripts/triage/comment_workflow.js
+++ scripts/triage/comment_workflow.js
@@ -10,6 +10,8 @@ async function handleComment(github, context) {
     const { issue, pull_request } = context.payload;
     const issueOrPullRequest = issue || pull_request;
 
+    console.log(issueOrPullRequest);
+
     const isMemberQuery = `query ($login: String!, $org: String!) {
         user(login: $login) {
           organization(login: $org) {
@@ -25,10 +27,10 @@ async function handleComment(github, context) {
     
     const isMemberResult = await github.graphql(isMemberQuery, isMemberVariables)
 
+    var isCommentFromMember = false;
+
     if (isMemberResult.user.organization != null) {
-        const isCommentFromMember = true;
-    } else { 
-        const isCommentFromMember = false;
+        isCommentFromMember = true;
     }
 
     //console.log(isCommentFromMember);
@@ -36,6 +38,53 @@ async function handleComment(github, context) {
     // If comment is from someone outside of the org
     if (!isCommentFromMember) {
         console.log('not from an org member')
+        //Get Item Info
+        const getItemInfoQuery = '
+            query($org: String!, $repo: String!, $project: Int!, $issue: Int!) {
+              organization(login: $org) {
+                repository(name: $repo) {
+                  issue(number: $issue) {
+                    projectItems(first: 10, includeArchived: false) {
+                      nodes {
+                        id
+                        fieldValueByName(name: "Status") {
+                          ... on ProjectV2ItemFieldSingleSelectValue {
+                            name
+                            field {
+                              ... on ProjectV2SingleSelectField {
+                                project {
+                                  ... on ProjectV2 {
+                                    id
+                                    number
+                                  }
+                                }
+                              }
+                            }
+                          }
+                        }
+                      }
+                    }
+                  }
+                }
+                projectV2(number: $project) {
+                  field(name: "Status") {
+                    ... on ProjectV2SingleSelectField {
+                      id
+                      options {
+                        id
+                        name
+                      }
+                    }
+                  }
+                }
+              }
+            }'
+        const getItemInfoVars = {
+            org: context.payload.organization.login,
+            repo: 
+            issueNumber:
+            projectNumber:
+        }
         // If issue is archived on the board, reactivate it
 
         // If the issue is open but is not on the project board, move it to the New Issues column on the project board
