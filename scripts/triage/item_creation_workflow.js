84ecf233f0b49a1263595588242bf67a22863be8
Ben M <benm@cypress.io>
2023-03-23 16:26:53 -0400

    chore: adding item creation workflow scripts

---
+++ scripts/triage/item_creation_workflow.js
@@ -0,0 +1,8 @@
+async function handleItemCreation(github, context) {
+
+    console.log(`github obj: ${ github }`);
+    console.log(`context obj: ${ context }`);
+
+};
+
+module.exports = { handleItemCreation };
