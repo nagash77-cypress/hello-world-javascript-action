9320e727e8ec4195f6157c86889b2fa23a86738f
Ben M <benm@cypress.io>
2023-04-18 10:13:59 -0400

    Update comment_workflow.js

--- scripts/triage/comment_workflow.js
+++ scripts/triage/comment_workflow.js
@@ -1,3 +1,5 @@
+const { isCypressOrgMember } = require('./isCypressMember')
+
 async function handleComment(github, context) {
   // Only continue if the payload was triggered by a comment event and the comment was made by a human user
   if (!context.payload.comment || context.payload.comment.user.type === 'Bot') {
@@ -16,25 +18,12 @@ async function handleComment(github, context) {
     ? { ...issue, type: 'pullRequest' }
     : { ...issue, type: 'issue' }
 
-  const isMemberQuery = `
-      query ($login: String!, $org: String!) {
-        user(login: $login) {
-          organization(login: $org) {
-            viewerIsAMember
-          }
-        }
-      }`
-  const isMemberVariables = {
-    login: context.payload.comment.user.login,
-    org: context.payload.organization.login,
-  }
+  let isCommentFromMember = await isCypressOrgMember(
+    github,
+    context.payload.comment.user.login
+  )
 
-  const isMemberResult = await github.graphql(isMemberQuery, isMemberVariables)
-
-  let isCommentFromMember = false
-
-  if (isMemberResult.user.organization != null) {
-    isCommentFromMember = true
+  if (isCommentFromMember) {
     console.log(
       `Comment came from a member of the Org.  isCommentFromMember var: ${isCommentFromMember}`
     )
@@ -166,6 +155,7 @@ async function handleComment(github, context) {
       )
       projectItemID = addToProjectBoard.addProjectV2ItemById.item.id
     }
+
     // If the issue is of status Closed on the project board, move it to the New Issues column
     if (issueDataFromGraphQL.closed) {
       const commentOnClosedItemQuery = `
