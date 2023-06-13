a8f5e6d1646cae176487273892f78419efef554a
Ben M <benm@cypress.io>
2023-06-13 16:07:28 -0400

    chore: updates to KPI generation

--- scripts/reports/generate_kpi_report.js
+++ scripts/reports/generate_kpi_report.js
@@ -1,5 +1,5 @@
 async function generateKPIReport(github, context, triageObjectString, mitigationObjectString) {
-
+  console.log('made it to the report function')
   let triageObject = JSON.parse(triageObjectString)
   let mitigationObjectString = JSON.parse(mitigationObjectString)
 
@@ -7,15 +7,15 @@ async function generateKPIReport(github, context, triageObjectString, mitigation
   console.log(`${mitigationObjectString}`)
   
   console.log('--------------------Total Open Issues--------------------')
-  console.log(`Repos Being Queried:  ${reposArray}`);
-  console.log(`Total Open Issues:  ${openIssueSum}`);
+  //console.log(`Repos Being Queried:  ${reposArray}`);
+  //console.log(`Total Open Issues:  ${openIssueSum}`);
   console.log('---------------------------------------------------------')
 
-  let closedIssueSum = getOpenedAndClosedIssueCount.search.nodes.reduce((total, node) => total + node.closedIssueCount.totalCount, 0)
+  //let closedIssueSum = getOpenedAndClosedIssueCount.search.nodes.reduce((total, node) => total + node.closedIssueCount.totalCount, 0)
 
   console.log('--------------------Total Closed Issues------------------')
-  console.log(`Repos Being Queried:  ${reposArray}`);
-  console.log(`Total Closed Issues:  ${closedIssueSum}`);
+  //console.log(`Repos Being Queried:  ${reposArray}`);
+  //console.log(`Total Closed Issues:  ${closedIssueSum}`);
   console.log('---------------------------------------------------------')
 
   return true
