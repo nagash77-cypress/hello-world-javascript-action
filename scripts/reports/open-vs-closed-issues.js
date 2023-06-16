1039a9aaaa18ec1e3e31eb27cdfd09262caea65d
Ben M <benm@cypress.io>
2023-06-16 10:09:59 -0400

    chore: fix action

--- scripts/reports/open-vs-closed-issues.js
+++ scripts/reports/open-vs-closed-issues.js
@@ -25,13 +25,8 @@ async function getOpenAndClosedIssueMetrics(github, context, core, reposArray) {
         }
       }
     `
-    // eventually I want to be able to just accept a normal list of repo names but I was having difficulty getting the github workflow to pass them nicely
-    //let repoNames = reposList 
     
     let orgName = context.payload.organization.login
-
-    // Split the string into an array of names
-    //let reposArray = repoNames.split(', ');
 
     // Prepend each name with "repo:SomeString/" and join them into a string
     let searchQuery = reposArray.map(name => `repo:${orgName}/${name}`).join(' ')
@@ -40,31 +35,10 @@ async function getOpenAndClosedIssueMetrics(github, context, core, reposArray) {
         searchQuery: searchQuery
     }
 
-    // console.log('----------------Query Params----------------------------')
-    // console.log(beginDate)
-    // console.log(endDate)
-    // console.log(reposArray)
-    // console.log(getOpenedAndClosedIssueCountParams)
-    // console.log(searchQuery);
-    // console.log('---------------------------------------------------------')
-
     const getOpenedAndClosedIssueCount = await github.graphql(getOpenedAndClosedIssueCountQuery, getOpenedAndClosedIssueCountParams)
 
-    //console.log(getOpenedAndClosedIssueCount.search.nodes)
-
     let openIssueSum = getOpenedAndClosedIssueCount.search.nodes.reduce((total, node) => total + node.openIssueCount.totalCount, 0)
-
-    console.log('--------------------Total Open Issues--------------------')
-    console.log(`Repos Being Queried:  ${reposArray}`);
-    console.log(`Total Open Issues:  ${openIssueSum}`);
-    console.log('---------------------------------------------------------')
-
     let closedIssueSum = getOpenedAndClosedIssueCount.search.nodes.reduce((total, node) => total + node.closedIssueCount.totalCount, 0)
-
-    console.log('--------------------Total Closed Issues------------------')
-    console.log(`Repos Being Queried:  ${reposArray}`);
-    console.log(`Total Closed Issues:  ${closedIssueSum}`);
-    console.log('---------------------------------------------------------')
 
     const issuesObject = {
       openIssueSum: openIssueSum,
