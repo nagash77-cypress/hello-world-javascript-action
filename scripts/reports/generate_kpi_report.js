492beed826978c43df9f996d9a6cdf57bb35d0c7
Ben M <benm@cypress.io>
2023-07-14 12:09:41 -0400

    chore: feature metrics

--- scripts/reports/generate_kpi_report.js
+++ scripts/reports/generate_kpi_report.js
@@ -1,4 +1,4 @@
-async function generateKPIReport(github, context, core, nonMonoStatsObject, monoStatsObjects, triageObject, mitigationObject) {
+async function generateKPIReport(github, context, core, nonMonoStatsObject, monoStatsObjects, triageObject, mitigationObject, featureObject) {
   
   console.log('--------------------Mono Repo Stats----------------------')
   console.log(`Repos Being Queried:  ${monoStatsObjects.reposArray}`);
@@ -13,8 +13,13 @@ async function generateKPIReport(github, context, core, nonMonoStatsObject, mono
   console.log('---------------------------------------------------------')
 
   console.log(`---------------------------Mitigation Metrics-------------------------------`)
-  console.log(`Triage Metrics (${mitigationObject.dateRange.startDate} - ${mitigationObject.dateRange.endDate})`)
+  console.log(`Mitigation Metrics (${mitigationObject.dateRange.startDate} - ${mitigationObject.dateRange.endDate})`)
   console.log(`Total Issues Provided With Workarounds: ${mitigationObject.issues.length}`)
+  console.log(`------------------------------------------------------------------------`)
+
+  console.log(`---------------------------Feature Metrics-------------------------------`)
+  console.log(`Feature Metrics (${featureObject.dateRange.startDate} - ${featureObject.dateRange.endDate})`)
+  console.log(`Total Feature Requests Submitted: ${featureObject.issues.length}`)
   console.log(`------------------------------------------------------------------------`)
 
   console.log(`---------------------------Triage Metrics-------------------------------`)
